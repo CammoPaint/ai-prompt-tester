32235ae4bd713ba2b11da4bfb9d2a2dadc6b8078
CammoPaint <moses_cn@hotmail.com>
2025-09-20 21:48:09 +1000

    Increase Workspace Settings popup width

--- src/components/chat/WorkspaceSettings.tsx
+++ src/components/chat/WorkspaceSettings.tsx
@@ -79,6 +79,7 @@ const WorkspaceSettings: React.FC<WorkspaceSettingsProps> = ({ workspace, onClos
   return (
     <div 
       className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4"
+      className="bg-white dark:bg-gray-900 rounded-lg shadow-xl max-w-2xl w-full max-h-[90vh] overflow-hidden"
       onClick={onClose}
     >
       <div 
